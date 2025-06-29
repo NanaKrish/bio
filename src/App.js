1d125fa1a1f45e2c186878c81e722e93bb6b56e8
NanaKrish <49097320+NanaKrish@users.noreply.github.com>
2025-06-29 01:31:01 +0000

    H15

--- src/App.js
+++ src/App.js
@@ -7,6 +7,6 @@ import Research from './pages/Research';
 import Experience from './pages/Experience';
 import Miscellaneous from './pages/Miscellaneous';
 function App() {
-    return (_jsx(Layout, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/education", element: _jsx(Education, {}) }), _jsx(Route, { path: "/research", element: _jsx(Research, {}) }), _jsx(Route, { path: "/experience", element: _jsx(Experience, {}) }), _jsx(Route, { path: "/miscellaneous", element: _jsx(Miscellaneous, {}) })] }) }));
+    return (_jsx(Layout, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/bio", element: _jsx(Home, {}) }), _jsx(Route, { path: "/education", element: _jsx(Education, {}) }), _jsx(Route, { path: "/research", element: _jsx(Research, {}) }), _jsx(Route, { path: "/experience", element: _jsx(Experience, {}) }), _jsx(Route, { path: "/miscellaneous", element: _jsx(Miscellaneous, {}) })] }) }));
 }
 export default App;
